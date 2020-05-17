6b8b1cd33331b08bb71e9b16c6519eb26f03c4e6
Elissa Simons <elissasimons@gmail.com>
2020-05-16 23:36:47 -0700

    Added for loop to randomize password

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,27 @@
-// Insert arrays w/char, sym, lower, upper, nums
+// Insert arrays with items that will randomize
 var char = ["!","@","#","$","%","^","&","*","(",")","-","+","_","=","{","}","[","]",";",":","~","`","'","?","/"];
 var nums = ["1","2","3","4","5","6","7","8","9","0"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var emptyArray = []
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var charArray = [];
+var password = '';
 
-// Insert prompt & confirms
-// If/else statement for length
-
-// Assignment Code
+var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// Add event listener to generate button
+generateBtn.addEventListener("click", function() {
+
+  //Add prompt & confirms for user input
+  alert('Hello! Please create a password that is between 8-128 characters that includes at least one of the following options.');
+
   var passwordLength = prompt('How many characters would you like your password to be?');
 
   if (passwordLength < 8) {
     alert('Your password must be at least 8 characters long.')
   }
 
-  else if (passwordLength > 128) {
+  if (passwordLength > 128) {
     alert('Your password cannot exceed 128 characters.')
   }
 
@@ -33,36 +35,32 @@ function writePassword() {
 
   // Push syms, nums, letters into empty array when confirmed in prompt
 
-  if (specialChars === true) {
-    emptyArray.push(char);
+  if (specialChars) {
+    charArray = charArray.concat(char);
    
   }
   
-  if (numbers === true) {
-    emptyArray.push(nums);
+  if (numbers) {
+    charArray = charArray.concat(nums);
     
   }
 
-  if (lowCase === true) {
-    emptyArray.push(lowerCase);
+  if (lowCase) {
+    charArray = charArray.concat(lowerCase);
     
   }
 
-  if (uppCase === true) {
-    emptyArray.push(upperCase);
+  if (uppCase) {
+    charArray = charArray.concat(upperCase);
   
   }
 
+  // Add for loop to randomize and create password
+  for (var i = 0; i < passwordLength; i++) {
+    var randomPassword = Math.floor(Math.random() * charArray.length);
+    password += charArray[randomPassword];
+  } 
 
+  document.getElementById('password').innerHTML = password;
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  // Write function to make it work
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+})
